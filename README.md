cbb86fa2525cc08068b173004d6c7ed0c7a31fb1
evenex <vlevenfeld@gmail.com>
2016-02-24 06:18:28 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# replizer
-Autogenerate a REPL over an object.
+# repl
+read-eval-print-loop
+
+Repl class is synchronized - once you go into a loop, you can't modify the repl bindings. Not sure if I want to go this way, long-term, but for now it will prevent some bugs.
+
+```d
+shared repl = new Repl;
+
+repl["add"] = (int a, int b) => a + b;
+repl["cat3"] = (string x) => x~x~x;
+
+repl["jk"] = () => 0xDEADBEEF;
+repl["jk"] = null;
+
+repl.loop;
+```
+
+```
+> hi
+commands: cat3, add
+> cat3
+cat3 expects (string), got ()
+> add
+add expects (int, int), got ()
+> add 5
+add expects (int, int), got (5)
+> add 5 10
+15
+> add 5 apple
+Unexpected 'p' when converting from type string to type int
+> cat3 9
+999
+> cat3 nine
+nineninenine
+```
---
+++ source/app.d
@@ -0,0 +1,69 @@
+import std.stdio;
+import std.range;
+import std.string;
+import std.algorithm;
+import std.functional;
+import std.typetuple;
+import std.conv;
+import std.concurrency;
+
+
+synchronized class Repl
+{
+  private string delegate(string[])[string] actions;
+
+  void opIndexAssign(B, A...)(B delegate(A) action, string name)
+  {
+    actions[name] = (string[] argStrs)
+    {
+      if(argStrs.length != A.length)
+        return text(
+          name, " expects ", A.stringof,
+          ", got (", argStrs.join(", "), ")"
+        );
+
+      A args;
+
+      foreach(i, ref arg; args)
+        arg = argStrs[i].to!(A[i]);
+
+      return action(args).text;
+    };
+  }
+  void opIndexAssign(typeof(null), string name)
+  {
+    actions.remove(name);
+  }
+
+  string read()
+  {
+    return stdin.readln!string;
+  }
+  string eval(string command)
+  {
+    auto cmd = command.split(" ").map!strip.filter!(not!empty);
+
+    if(cmd.empty)
+      return help;
+    if(auto action = cmd.front in actions)
+      try 
+        return (*action)(cmd.dropOne.array);
+      catch(Exception ex)
+        return ex.msg;
+    else
+      return help;
+  }
+  void print(string result)
+  {
+    result.writeln;
+  }
+  void loop()
+  {
+    while(1)
+      print(eval(read));
+  }
+  string help()
+  {
+    return text("commands: ", actions.keys.join(", "));
+  }
+}
